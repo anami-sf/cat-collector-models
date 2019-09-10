489e7733b567b2c498933791ae693493ccbe257f
anami823 <anami823@gmail.com>
2019-09-09 21:02:54 -0700

    Create admin superuser and register cats model

--- main_app/models.py
+++ main_app/models.py
@@ -6,3 +6,8 @@ class Cat(models.Model):
     age = (models.CharField(max_length=5))
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
+
+def _str_(self):
+    return self.name
+
+    
