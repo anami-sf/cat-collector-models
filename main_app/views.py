a58bbcffe5a3c33d6f99d08b54bceebddf5aa854
anami823 <anami823@gmail.com>
2019-09-10 11:01:45 -0700

    Define update and delete views

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Cat
-#from django.views.generic import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # class CatList(ListView):
 #     model = Cat
@@ -29,3 +28,14 @@ class CatCreate(CreateView):
     model = Cat
     fields = ['name', 'breed', 'description', 'age']
     # fields = '__all__'  same as ^^^
+
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
